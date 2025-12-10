d3115af6eec1cbb9665477d6bbcd78bd2d34c3d5
Jahan Gagan <jahanggn9@gmail.com>
2025-12-10 20:45:35 +0530

    feat: newest approach

--- plugin.rb
+++ plugin.rb
@@ -14,7 +14,9 @@ module ::DiscourseIpAnonymizer
   def self.anonymize_ip(real_ip)
     Rails.logger.warn "[IP-ANON] anonymize_ip called with: #{real_ip}"
     secret_key = SiteSetting.ip_anonymizer_secret_key
+    Rails.logger.warn "[IP-ANON] Secret key length: #{secret_key&.length}"
     hmac = OpenSSL::HMAC.hexdigest('SHA256', secret_key, real_ip)
+    Rails.logger.warn "[IP-ANON] HMAC: #{hmac[0..15]}..."
     hex_parts = hmac[0..7].scan(/.{2}/)
     ip_parts = hex_parts.map { |hex| hex.to_i(16) }
     result = ip_parts.join('.')
@@ -22,13 +24,27 @@ module ::DiscourseIpAnonymizer
     result
   end
   
+  # Patch Rack::Request#ip
   module RackRequestIpPatch
     def ip
-      Rails.logger.warn "[IP-ANON] RackRequestIpPatch#ip method called"
+      Rails.logger.warn "[IP-ANON] Rack::Request#ip called"
       real_ip = super
-      Rails.logger.warn "[IP-ANON] Real IP from super: #{real_ip}"
+      Rails.logger.warn "[IP-ANON] Rack::Request#ip super returned: #{real_ip}"
       anonymized = ::DiscourseIpAnonymizer.anonymize_ip(real_ip)
-      Rails.logger.warn "[IP-ANON] Returning anonymized IP: #{anonymized}"
+      Rails.logger.warn "[IP-ANON] Rack::Request#ip returning: #{anonymized}"
+      anonymized
+    end
+  end
+
+  # Patch ActionDispatch::Request#remote_ip
+  module ActionDispatchRequestRemoteIpPatch
+    def remote_ip
+      Rails.logger.warn "[IP-ANON] ActionDispatch::Request#remote_ip called"
+      Rails.logger.warn "[IP-ANON] Caller: #{caller[0..2].join(' | ')}"
+      real_ip = super
+      Rails.logger.warn "[IP-ANON] ActionDispatch::Request#remote_ip super returned: #{real_ip}"
+      anonymized = ::DiscourseIpAnonymizer.anonymize_ip(real_ip.to_s)
+      Rails.logger.warn "[IP-ANON] ActionDispatch::Request#remote_ip returning: #{anonymized}"
       anonymized
     end
   end
@@ -37,7 +53,18 @@ end
 require_relative "lib/discourse_ip_anonymizer/engine"
 
 after_initialize do
-  Rails.logger.warn "[IP-ANON] Patching Rack::Request"
+  Rails.logger.warn "[IP-ANON] === STARTING INITIALIZATION ==="
+  
+  # Patch Rack::Request
+  Rails.logger.warn "[IP-ANON] Patching Rack::Request#ip"
   Rack::Request.prepend(::DiscourseIpAnonymizer::RackRequestIpPatch)
-  Rails.logger.warn "[IP-ANON] Rack::Request ancestors: #{Rack::Request.ancestors.first(5).join(', ')}"
+  Rails.logger.warn "[IP-ANON] Rack::Request ancestors: #{Rack::Request.ancestors.first(7).join(', ')}"
+  
+  # Patch ActionDispatch::Request
+  Rails.logger.warn "[IP-ANON] Patching ActionDispatch::Request#remote_ip"
+  ActionDispatch::Request.prepend(::DiscourseIpAnonymizer::ActionDispatchRequestRemoteIpPatch)
+  Rails.logger.warn "[IP-ANON] ActionDispatch::Request ancestors: #{ActionDispatch::Request.ancestors.first(7).join(', ')}"
+  
+  Rails.logger.warn "[IP-ANON] === INITIALIZATION COMPLETE ==="
 end
+
